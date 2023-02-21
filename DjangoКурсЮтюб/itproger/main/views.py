61e32c036624a3559dc58ea4f106c4307edbb839
W83w <ya.belov-andreq@yandex.ru>
2023-02-21 16:57:17 +0400

    django project
    The site is being developed on the itproger course.

---
+++ DjangoКурсЮтюб/itproger/main/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    data = {
+        'title': 'Главная страница',
+        'values': ['Some', 'Hello', '123'],
+        'obj': {
+            'car': 'BMW',
+            'age': 18,
+            'hobby': 'Football'
+        }
+    }
+    return render(request, 'main/index.html', data)
+
+
+def about(request):
+    return render(request, 'main/about.html')
+
+
+def prochee(request):
+    return render(request, 'main/prochee.html')
